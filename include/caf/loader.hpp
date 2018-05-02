3ca85286a34cbdcd8572ca9aeaa15a883982ae30
Comfy kernel <comfykernel@gmail.com>
2018-05-02 08:25:13 -0700

    Moved files around and added basic CAF classes

---
+++ include/caf/loader.hpp
@@ -0,0 +1,13 @@
+#ifndef CAF_LOADER_HPP
+#define CAF_LOADER_HPP
+
+#include <vector>
+#include "types.hpp"
+
+namespace caf {
+  class loader {
+  public:
+  }
+}
+
+#endif
---
+++ include/caf/types.hpp
@@ -0,0 +1,51 @@
+#ifndef CAF_TYPES_HPP
+#define CAF_TYPES_HPP
+
+#include <cstdint>
+#include <string>
+
+namespace caf {
+  struct flags {
+    bool unused7 : 1; // BYTE 0
+    bool unused6 : 1;
+    bool unused5 : 1;
+    bool unused4 : 1;
+    bool file    : 1;
+    bool empty   : 1;
+    bool ignore  : 1;
+    bool item    : 1;
+    
+    bool type7 : 1; // BYTE 1
+    bool type6 : 1;
+    bool type5 : 1;
+    bool type4 : 1;
+    bool type3 : 1;
+    bool type2 : 1;
+    bool type1 : 1;
+  }
+  
+  struct lumpitem {
+    union {
+      uint16_t all;
+      flags bits;
+    } flags;
+    
+    uint32_t ptr_lump;    
+    uint16_t ver_major;
+    uint16_t ver_minor;
+    uint32_t ptr_lumpitem;
+    std::string name;
+    std::string path;
+    std::string type;
+  };
+
+  class caf {
+    uint16_t ver_major;
+    uint16_t ver_minor;
+    uint32_t revision;
+    uint32_t ptr_lumpitem;
+    std::string root;
+  };
+}
+
+#endif
